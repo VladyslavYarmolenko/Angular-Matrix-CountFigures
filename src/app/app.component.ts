4ea7860ba34992ab6cf807b7a3009845c8a7c8ef
Vladyslav <vladedumail@gmail.com>
2021-10-05 12:46:54 +0300

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularMatrixFigureCounter';
+  figuresCount: number = 0;
+
+  matrixArray: string[][] = [
+    ['0', '0', '0', '1', '1', '0', '0', '0'],
+    ['0', '1', '0', '0', '1', '1', '1', '0'],
+    ['0', '1', '1', '0', '0', '0', '1', '0'],
+    ['0', '0', '0', '0', '0', '0', '1', '0'],
+    ['1', '0', '1', '1', '1', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '1', '0']
+  ]
+
+  resultArr = this.matrixArray.map(item => [...item])
+
+  markNeighbourElems(matrix: any, row: number, col: number): void {
+    matrix[row][col] = '5';
+    if (matrix[row][col - 1] === '1'){
+      this.markNeighbourElems(matrix, row, col - 1)
+    }
+
+    if (matrix[row][col + 1] === '1'){
+      this.markNeighbourElems(matrix, row, col + 1)
+    }
+
+    if (matrix?.[row - 1]?.[col] === '1'){
+      this.markNeighbourElems(matrix, row - 1 , col)
+    }
+
+    if (matrix?.[row + 1]?.[col] === '1'){
+      this.markNeighbourElems(matrix, row + 1, col + 1)
+    }
+  }
+
+  numOfIslands(matrix: any): number {
+    let counter = 0;
+
+    if (matrix.length === 0) {
+      this.figuresCount = 0;
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+      for(let k = 0; k < matrix[i].length; k++){
+        if (matrix[i][k] === '1'){
+          counter++;
+          this.markNeighbourElems(matrix, i, k);
+        }
+      }
+    }
+    return counter;
+  }
+
+  ngOnInit() {
+    this.figuresCount = this.numOfIslands(this.resultArr);
+  }
 }
